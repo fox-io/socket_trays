928912ecaaf2638c67b3d4384507f7f8dca589c8
John M. <github.fox-io@foxdata.io>
2024-09-24 17:05:01 -0500

    Adjust spacing and tolerances. Fix comments.

--- socket_trays.py
+++ socket_trays.py
@@ -1,23 +1,25 @@
 import os
 
-socket_widths = [18.0, 17.5, 16.25, 15.25, 13.5]
-socket_heights = [50.25, 50.25, 50.25, 50.25, 50.25]
+socket_widths = [31, 29, 27, 25.5, 23]
+socket_heights = [64.75, 64.75, 57, 57, 54.5]
 socket_offsets = []
 border_width = 5
-base_height = 3
-socket_spacing = 0.25
+base_height = 5
+socket_spacing = 0.5
 path_to_openscad = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
 path_to_stl = "/Users/raven/PycharmProjects/socket_trays/sockets.stl"
 path_to_scad = "/Users/raven/PycharmProjects/socket_trays/sockets.scad"
 
+# Calculate socket_offsets
 i = -1
 for width in socket_widths:
     i += 1
     if i == 0:
-        socket_offsets.append(width/2 + border_width)
+        socket_offsets.append((width/2) + border_width)
     else:
-        socket_offsets.append(width/2 + socket_widths[i-1]/2 + socket_offsets[i-1] + socket_spacing)
+        socket_offsets.append((width/2) + (socket_widths[i-1]/2) + socket_offsets[i-1] + socket_spacing)
 
+# Calculate tray size
 tray_width = 0
 for width in socket_widths:
     tray_width += width
@@ -26,9 +28,9 @@ tray_width += border_width * 2
 
 tray_depth = socket_heights[0] + border_width * 2
 
-tray_height = socket_widths[0]/2 + base_height
+tray_height = (socket_widths[0]/2) + base_height
 
-# Example output
+# Create .scad contents
 output = f'''
 socket_widths = {socket_widths};
 socket_heights = {socket_heights};
@@ -49,19 +51,22 @@ difference()
                 translate([socket_offsets[i], socket_widths[i]/2.0 + base_height, -1.0 * (socket_heights[i] + border_width)])
                     cylinder(socket_heights[i], d=socket_widths[i], true);
         for(i = [0:1:len(socket_widths)-1])
-            translate([socket_offsets[i], (socket_heights[i]/4)* 1 + border_width, 0.75])
-                cylinder(3, d=6.5, true);
+            translate([socket_offsets[i], (socket_heights[i]/4)* 1 + border_width, 2.25])
+                cylinder(3.25, d=7, true);
         for(i = [0:1:len(socket_widths)-1])
-            translate([socket_offsets[i], (socket_heights[i]/4)* 3 + border_width, 0.75])
-                cylinder(3, d=6.5, true);
+            translate([socket_offsets[i], (socket_heights[i]/4)* 3 + border_width, 2.25])
+                cylinder(3.25, d=7, true);
         for(i = [0:1:len(socket_widths)-1])
             rotate([90, 0, 0])
                 translate([socket_offsets[i], 0, -1 * ((socket_heights[i]/2) + border_width)])
-                    cube([3, 3, 33], true);
+                    cube([5, 5, 34.5], true);
     }}
 }}'''
 
+# Write .scad file
 outfile = open(path_to_scad, "w")
 outfile.write(output)
 outfile.close()
+
+# Convert to .stl
 os.system(f"open -a {path_to_openscad} --args -o {path_to_stl} {path_to_scad}")
