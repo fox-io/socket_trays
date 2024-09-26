25da25227660a1cddfb0d165019d0fac2307c23d
John M. <github.fox-io@foxdata.io>
2024-09-25 22:34:11 -0500

    Grouped variables. Adjusted scad string to simplify output. Adjusted shunt channel and magnet holes.

--- socket_trays.py
+++ socket_trays.py
@@ -1,41 +1,53 @@
 import os
 
-socket_widths = [31, 29, 27, 25.5, 23]
-socket_heights = [64.75, 64.75, 57, 57, 54.5]
-socket_offsets = []
-border_width = 5
-base_height = 7
-socket_spacing = 0.5
-path_to_openscad = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD'
-path_to_stl = "/Users/raven/PycharmProjects/socket_trays/sockets.stl"
-path_to_scad = "/Users/raven/PycharmProjects/socket_trays/sockets.scad"
+sockets = {
+    'widths': [31, 29, 27, 25.5, 23],
+    'heights': [64.75, 64.75, 57, 57, 54.5],
+    'spacing': 0.5,
+    'offsets': []
+}
+base = {
+    'border': 5,
+    'bottom': 3
+}
+magnet = {
+    'diameter': 6.5,
+    'height': 2.5
+}
+shunt = {
+    'width': 5,
+    'height': 5,
+    'length': 34.5
+}
+paths = {
+    'openscad': '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD',
+    'stl': '/Users/raven/PycharmProjects/socket_trays/sockets.stl',
+    'scad': '/Users/raven/PycharmProjects/socket_trays/sockets.scad'
+}
 
 # Calculate socket_offsets
-for i in range(0, len(socket_widths)):
+for i in range(0, len(sockets['widths'])):
     if i == 0:
-        socket_offsets.append((socket_widths[i]/2) + border_width)
+        sockets['offsets'].append((sockets['widths'][i]/2) + base['border'])
     else:
-        socket_offsets.append((socket_widths[i]/2) + (socket_widths[i-1]/2) + socket_offsets[i-1] + socket_spacing)
+        sockets['offsets'].append((sockets['widths'][i]/2) + (sockets['widths'][i-1]/2) + sockets['offsets'][i-1] + sockets['spacing'])
 
 # Calculate tray size
 tray_width = 0
-for width in socket_widths:
+for width in sockets['widths']:
     tray_width += width
-tray_width += (len(socket_widths)-1) * socket_spacing
-tray_width += border_width * 2
+tray_width += (len(sockets['widths'])-1) * sockets['spacing']
+tray_width += base['border'] * 2
 
-tray_depth = socket_heights[0] + border_width * 2
+tray_depth = sockets['heights'][0] + base['border'] * 2
 
-tray_height = (socket_widths[0]/2) + base_height
+tray_height = (sockets['widths'][0]/2) + base['bottom'] + shunt['height'] + magnet['height']
 
 # Create .scad contents
 output = f'''
-socket_widths = {socket_widths};
-socket_heights = {socket_heights};
-socket_offsets = {socket_offsets};
-
-border_width = {border_width};
-base_height = {base_height};
+socket_widths = {sockets['widths']};
+socket_heights = {sockets['heights']};
+socket_offsets = {sockets['offsets']};
 
 difference()
 {{
@@ -45,25 +57,25 @@ difference()
     {{
         for(i = [0:1:len(socket_widths)-1])
             rotate([90, 0, 0])
-                translate([socket_offsets[i], {tray_height}, -1.0 * (socket_heights[i] + border_width)])
+                translate([socket_offsets[i], {tray_height}, -1.0 * (socket_heights[i] + {base['border']})])
                     cylinder(socket_heights[i], d=socket_widths[i], true);
         for(i = [0:1:len(socket_widths)-1])
-            translate([socket_offsets[i], (socket_heights[i]/4)* 1 + border_width, {tray_height} - (socket_widths[i]/2) - 2.25])
-                cylinder(3.25, d=7, true);
+            translate([socket_offsets[i], (socket_heights[i]/4)* 1 + {base['border']}, {tray_height} - (socket_widths[i]/2) - {magnet['height'] - 0.5}])
+                cylinder({magnet['height']}, d={magnet['diameter']}, true);
         for(i = [0:1:len(socket_widths)-1])
-            translate([socket_offsets[i], (socket_heights[i]/4)* 3 + border_width, {tray_height} - (socket_widths[i]/2) - 2.25])
-                cylinder(3.25, d=7, true);
+            translate([socket_offsets[i], (socket_heights[i]/4)* 3 + {base['border']}, {tray_height} - (socket_widths[i]/2) - {magnet['height'] - 0.5}])
+                cylinder({magnet['height']}, d={magnet['diameter']}, true);
         for(i = [0:1:len(socket_widths)-1])
             rotate([90, 0, 0])
-                translate([socket_offsets[i], {tray_height} - (socket_widths[i]/2) - 3, -1 * ((socket_heights[i]/2) + border_width)])
-                    cube([5, 7, 34.5], true);
+                translate([socket_offsets[i], {tray_height} - (socket_widths[i]/2) - {magnet['height']/2}, -1 * ((socket_heights[i]/2) + {base['border']})])
+                    cube([{shunt['width']}, {shunt['height'] + magnet['height']}, {shunt['length']}], true);
     }}
 }}'''
 
 # Write .scad file
-outfile = open(path_to_scad, "w")
+outfile = open(paths['scad'], "w")
 outfile.write(output)
 outfile.close()
 
 # Convert to .stl
-os.system(f"open -a {path_to_openscad} --args -o {path_to_stl} {path_to_scad}")
+os.system(f"open -a {paths['openscad']} --args -o {paths['stl']} {paths['scad']}")
